303e54468556e93caec34daebef9d2fa697061da
soncrayon <arnold.curtis@gmail.com>
2020-11-23 22:51:37 -0500

    create card faces and randomize card placement

--- matching_game_aero.js
+++ matching_game_aero.js
@@ -1,15 +1,83 @@
-const createCard = () => {
-    return document.createElement('div').innerHTML = "testing out the scenes right here.  i guess it will just keep going"
+const createCardBackImage = () => {
+    let cardBackImage = document.createElement('img')
+    cardBackImage.src = 'https://img.icons8.com/officel/16/000000/edit.png'
+    cardBackImage.alt = 'card back image'
+    return cardBackImage
+}
+
+const createCardFrontImage = (image) => {
+    let cardFrontImage = document.createElement('img')
+    cardFrontImage.src = image
+    cardFrontImage.alt = "card front image"
+    return cardFrontImage
+}
+
+const createCardBack = () => {
+    let cardBack = document.createElement('div')
+    cardBack.className = "card_face card_face_back"
+    cardBack.append(createCardBackImage())
+    return cardBack
+}
+
+const createCardFront = (image) => {
+    let cardFront = document.createElement('div')
+    cardFront.className = "card_face card_face_front"
+    cardFront.append(createCardFrontImage(image))
+    return cardFront
+}
+
+const toggleCardFlip = (event) => {
+    console.log (event.target.className)
+    // if (event.target.className = "card animate__animated animate__flip") {
+    //     return event.target.className = "card animate__animated animate__flip" 
+    // } 
+    // return event.target.className = "card animate__animated animate__flip"
+}
+
+const createCard = (obj) => {
+    let card = document.createElement('div')
+    card.className = "card animate__animated animate__flipInY"
+    card.id = obj.id
+    card.innerHTML = "I need some text here to get the card to open up I think.  This will be replaced by an image later."
+    card.append(createCardFront(obj.image_url))
+    card.append(createCardBack())
+    card.addEventListener('click', (event) => {
+        toggleCardFlip(event)
+    })
+    return card
+}
+
+const getCardImageObjects = () => {
+    let cardObjects = [
+            {id: 1, image_url: "https://img.icons8.com/officel/16/000000/sun.png"},
+            {id: 2, image_url: "https://img.icons8.com/officel/16/000000/toolbox.png"},
+            {id: 3, image_url: "https://img.icons8.com/officel/16/000000/music.png"},
+            {id: 4, image_url: "https://img.icons8.com/officel/16/000000/binoculars.png"},
+            {id: 5, image_url: "https://img.icons8.com/officel/16/000000/key.png"},
+            {id: 6, image_url: "https://img.icons8.com/officel/16/000000/idea.png"},
+            {id: 7, image_url: "https://img.icons8.com/officel/16/000000/checkmark.png"},
+            {id: 8, image_url: "https://img.icons8.com/officel/16/000000/home.png"}
+    ]
+    for (let i = cardObjects.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i)
+        const temp = cardObjects[i]
+        cardObjects[i] = cardObjects[j]
+        cardObjects[j] = temp
+    }
+    console.log(cardObjects)
+    return cardObjects
 }
 
 const createCardScenes = () => {
     const cardScenes = []
     let cardScene
-    for (let i = 0; i < 16; i++) {
-        cardScene = document.createElement('div')
-        cardScene.className = 'card_scene'
-        cardScene.append(createCard())
-        cardScenes.push(cardScene)
+    for (let i = 0; i < 2; i++) {
+        getCardImageObjects().forEach((obj) => {
+            cardScene = document.createElement('div')
+            cardScene.className = 'card_scene'
+            cardScene.append(createCard(obj))
+            cardScenes.push(cardScene)
+        }) 
     }
     return cardScenes
 }
